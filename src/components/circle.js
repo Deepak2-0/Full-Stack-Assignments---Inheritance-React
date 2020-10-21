1f2887faf94ab1c8981c5471219c4df0d0faeba0
Deepak Choudhary <choudharydeepak938@gmail.com>
2020-10-21 16:45:19 +0530

    commented

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,9 +1,9 @@
 import Shape from "Shape";
 
 class Cirlce extends Shape {
-  constructor(color) {
-    super(color);
-  }
+  // constructor(color) {
+  //   super(color);
+  // }
 
   calculateArea() {
     console.log("area of circle");
